bd5f835faf8901c5a07eb17e243d27f516f933a1
Deoniks <55949787+Deoniks@users.noreply.github.com>
2022-11-17 14:03:48 +0600

    upd. const.py

--- const.py
+++ const.py
@@ -10,19 +10,3 @@ cursor = conn.cursor()
 order_menu = ['хлеб', 'булочки', '/пирожные', '/чай', '/молоко', '/кофе']
 order_count = ['/1', '/2' '/3'
                      '/4', '/5', '/10']
-
-
-@bot.message_handler(commands=order_menu)
-def mess(message):
-    n = message.text
-    user_markup = telebot.types.ReplyKeyboardMarkup(True, False)
-    user_markup.row('хлеб', 'булочки', 'пирожные')
-    bot.send_message(message.from_user.id, 'вводим', reply_markup=user_markup)
-    print(n)
-
-bot.polling(non_stop=True, interval=0)
-
-"""if bread in order_menu:
-    print('ok')
-else:
-    print('dont no')"""
